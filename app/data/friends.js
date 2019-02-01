a9cb50b4c5036416936c71a326470241384f23c0
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-01-31 22:16:43 -0700

    survey

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends = {
+    
+    "name": "Charlie",
+    "photo": "../public/assets/images/charlie.jpeg",
+    "scores": [
+        1,
+        2,
+        4,
+        5,
+        6,
+        8,
+        2,
+        7,
+        5,
+        9
+    ]
+}

32be77e732b079238fee0d7cd8278188cedbac2f
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-02-05 20:23:27 -0700

    meat of project

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
-var friends = {
+var friends = [
+    {
     
     "name": "Charlie",
     "photo": "../public/assets/images/charlie.jpeg",
@@ -7,11 +8,88 @@ var friends = {
         2,
         4,
         5,
-        6,
-        8,
+        4,
         2,
-        7,
+        2,
+        3,
         5,
-        9
+        1
     ]
-}
+    
+    },
+    {
+    
+        "name": "Mac",
+        "photo": "../public/assets/images/mac.png",
+        "scores": [
+            5,
+            2,
+            4,
+            5,
+            3,
+            1,
+            2,
+            4,
+            5,
+            2
+        ]
+        
+    },
+    {
+    
+        "name": "Dee",
+        "photo": "../public/assets/images/dee.jpeg",
+        "scores": [
+            3,
+            2,
+            4,
+            1,
+            3,
+            4,
+            5,
+            4,
+            5,
+            2
+        ]
+        
+    },
+    {
+    
+        "name": "Dennis",
+        "photo": "../public/assets/images/denis.jpeg",
+        "scores": [
+            1,
+            2,
+            1,
+            1,
+            5,
+            4,
+            5,
+            4,
+            3,
+            2
+        ]
+        
+    },
+    {
+    
+        "name": "Frank",
+        "photo": "../public/assets/images/frank.jpeg",
+        "scores": [
+            5,
+            5,
+            3,
+            1,
+            2,
+            4,
+            3,
+            4,
+            2,
+            1
+        ]
+        
+    }
+];
+
+    
+module.exports = friends;
